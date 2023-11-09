9418258fd8d3680dc51fd0f4f6c3ea84ef05c0ce
Kieren <kierenhuynh88@gmail.com>
2023-11-09 16:56:11 +1100

    ok

--- overload/src/app/home/page.tsx
+++ overload/src/app/home/page.tsx
@@ -1,11 +1,21 @@
+import SelectedCourses from "../components/selectedCourses";
+import CoursesList from "../components/coursesList";
+
 export const Home = () => {
     return (
-        <div className="flex">
-            <div className="w-7/10">
-                left
+        <div className="bg-gray-500 h-[100vh]">
+            <div className="text-center h-[20vh]">
+                top
             </div>
-            <div className="w-3/10">
-                right
+            <div className="flex pr-[8vw] pl-[8vw]">
+                <div className="w-[70%]">
+                    {/* Content for the 70% width div */}
+                    <SelectedCourses />
+                </div>
+                <div className="w-[30%] bg-black">
+                    {/* Content for the 30% width div */}
+                    <CoursesList />
+                </div>
             </div>
         </div>
     )

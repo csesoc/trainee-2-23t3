2df82da41140eae3ecbbf32ab0145a653b3e24e7
Kieren <kierenhuynh88@gmail.com>
2023-11-09 15:41:38 +1100

    start

---
+++ overload/src/app/home/page.tsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return (
+        <div className="flex">
+            <div className="w-7/10">
+                left
+            </div>
+            <div className="w-3/10">
+                right
+            </div>
+        </div>
+    )
+}
+
+export default Home;

32911e05f827c336960e3ed8a8c0f1f0dbc66dae
FlyingChilli04 <lmaisnam2@gmail.com>
2023-11-21 12:58:15 +1100

    Added temporary search bar using temp data

---
+++ overload/src/app/components/SearchBar.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React from 'react'
+
+export const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [dataList, setDataList] = React.useState([]);
+
+  React.useEffect(() => {
+    // Fetch the list of Pokémon from the PokeAPI (temporary)
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151') // Fetch the first 151 Pokémon (temp data)
+      .then((response) => response.json()) // turn data from promise into a json file so we can use data
+      .then((data) => setDataList(data.results)) // set data using setData hook
+      .catch(error => {
+        // if promise not fulfilled
+        console.log('Error:' + `${error.message}`);
+      });
+  }, []);
+
+  const filteredData = dataList.filter((item) =>
+    (item as { name: string }).name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="text-center py-4">
+      <input className="bg-white py-0.1 text-center text-0.5xl border border-black rounded-lg p-.5" type="text" placeholder="search a course" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}/>
+      <ul className="">
+        {filteredData.map((item) => (
+          <li key={(item as { name: string }).name}>{(item as { name: string }).name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
---
+++ overload/src/app/course-rating/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { SearchBar } from "../components/SearchBar"
+
+export default function CourseRatingPage() {
+  return (
+    <div>
+      {/* <NavBar/> */}
+      <h1 className="font-sans text-5xl text-red-500 font-semibold text-center">OverLoad</h1>
+      <SearchBar/>
+      {/* <SortButton/> */}
+      
+    </div>
+  )
+}
+
+// Plan
+// navbar
+// search bar (look at to do list for inspo) 
+// array of CourseCards
+// sort button
+// 
+

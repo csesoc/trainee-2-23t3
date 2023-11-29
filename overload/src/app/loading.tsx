e0c42130f7ae2b59565d48574d2b6e0d17c43fe0
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-29 19:17:06 +1100

    fix loading screen

--- overload/src/app/loading.tsx
+++ overload/src/app/loading.tsx
@@ -1,10 +1,11 @@
 import './loading.css'
 import skullSvg from './resources/skull-head-svgrepo-com.svg';
+import Image from 'next/image'
 
 export default function Loading() {
   return (
     <div className="min-h-screen flex justify-center items-center  flex-col">
-      <img src={skullSvg} alt="OverLoad" className="animate-bounce-spin"/>
+      <Image src={skullSvg} alt="OverLoad" className="animate-bounce-spin"/>
     </div>
   )
 }

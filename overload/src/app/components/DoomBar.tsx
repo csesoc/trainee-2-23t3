a41171fa0054b82e476d664d20515342d102177c
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-12-12 20:47:39 +0700

    add more skulls

--- overload/src/app/components/DoomBar.tsx
+++ overload/src/app/components/DoomBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import skullSvg from '@/app/resources/skull-head-svgrepo-com.svg'
+import skullSvg from '@/app/resources/skull-head-svgrepo-com.svg';
 import Image from 'next/image';
 
 type DoomBarProps = {
@@ -14,8 +14,17 @@ export default function DoomBar({ doomness }: DoomBarProps) {
   };
 
   return (
-    <div className={`w-full h-16 mt-10 flex items-center justify-center ${doomColors[doomness]}`}>
-      <Image src={skullSvg} alt='skull-logo' className='object-cover h-16 w-16'/>
+    <div
+      className={`w-full h-16 mt-10 flex items-center justify-evenly ${doomColors[doomness]}`}
+    >
+      {Array.from({length: 5}).map((_, idx) => (
+        <Image
+          src={skullSvg}
+          alt="skull-logo"
+          className="object-cover h-16 w-16"
+          key={idx}
+        />
+      ))}
     </div>
   );
 }
